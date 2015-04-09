19e5caad42553c386e7843d789e199d942117947
Ruslan Shevtsov <r.shevtsov@aericon.com>
2015-04-09 14:03:30 +0300

    Initial Commit

---
+++ VideoTransitionsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoTransitionsSample
+//
+//  Created by Ruslan Shevtsov on 4/9/15.
+//  Copyright (c) 2015 iQueSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoTransitionsSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoTransitionsSample
+//
+//  Created by Ruslan Shevtsov on 4/9/15.
+//  Copyright (c) 2015 iQueSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
